9c77cf411aa678670ba3128c56c84d140d1aed8a
Baldebo, Emil <ebaldebo@gmail.com>
2025-03-11 19:19:30 +0100

    added context timeout

--- app/app.go
+++ app/app.go
@@ -38,7 +38,9 @@ func Run() {
 			continue
 		}
 
-		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
 		suggestion, err := suggester.Suggest(ctx, input)
 		if err != nil {
 			log.Printf("failed to suggest: %v", err)
